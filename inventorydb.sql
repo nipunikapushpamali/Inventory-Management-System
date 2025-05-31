e313f30647c15c915cad0a8886d83ff908f74a03
nipunikapushpamali <135969612+nipunikapushpamali@users.noreply.github.com>
2025-06-01 00:23:42 +0530

    Update inventorydb.sql

--- inventorydb.sql
+++ inventorydb.sql
@@ -1,14 +1,14 @@
 
-CREATE DATABASE IF NOT EXISTS inventorydb;
+CREATE DATABASE inventorydb;
 USE inventorydb;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS inventory (
+CREATE TABLE inventory (
     id INT AUTO_INCREMENT PRIMARY KEY,
     item_name VARCHAR(100),
     item_code VARCHAR(50),
